c8480255a66a9c82dfee1c0ae4f17f2a18e891a7
Nitish Anand <nanan3@sapient.com>
2017-12-29 17:43:28 +0530

    banner image added

--- ann-taylor/src/components/MainBanner.js
+++ ann-taylor/src/components/MainBanner.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react';
 import { Container, Row } from 'reactstrap';
+import YouTube from 'react-youtube';
+
+
+class BannerImg extends React.Component {
+constructor(props) {
+		super(props);
+		this.state = {
+			showPoster: true
+		};
+	}
+	hideMainBannerImage(){
+		console.log("hello");
+		console.log();
+	}
+  render() {
+    return (
+		<div>
+			<Container>
+				<Row>
+					<img 
+						src="https://www.anntaylor.com/webassets/content/ann/landing/luxewear/luxewear_hdr_desktop_091517.jpg" 
+						alt="India" 
+						className="img-responsive"
+						onClick={this.hideMainBannerImage}
+					/>
+				</Row>
+			</Container>
+		</div>
+    );
+  }
+} 
 
 
 class MainBanner extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			showPoster: false,
+			showPoster: true
 		};
 	}
 
 	render() {
-		
+		const opts = {
+			height: '390',
+			playerVars: {
+				autoplay: 1
+			}
+		};
+
 		return(
 			<div>
 				<Container>
 					<Row>
 						<div className="main_banner_block">
-							<img 
-								src="https://www.anntaylor.com/webassets/content/ann/landing/luxewear/luxewear_hdr_desktop_091517.jpg" 
-								alt="India" 
-								className="img-responsive"
-								onLoad={console.log(this.state.showPoster)}
+							{ this.state.showPoster ? <BannerImg /> : null }
+
+							<YouTube
+								videoId="vfj0rObjO_M"
+								opts={opts}
+								onReady={this._onReady}
 							/>
 						</div>
 					</Row>
@@ -29,6 +67,9 @@ class MainBanner extends Component {
 			</div>
 		);
 	};
+	_onReady(event) {
+		event.target.pauseVideo();
+	}
 };
 
 export default MainBanner;
