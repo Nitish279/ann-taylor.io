24053901e8f0bf918b5843b80cfa93f81eccba01
Nitish Anand <nanan3@sapient.com>
2018-01-03 14:01:58 +0530

    made left nav dynamic

--- ann-taylor/src/components/LeftNavigation.js
+++ ann-taylor/src/components/LeftNavigation.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
+import data from '../data/data.json';
 
 class LeftNavigation extends Component {
+
+	constructor(props) {
+		super(props);
+
+		const categoryDetails = data.resultsList.nav;
+		const categoryDetailsArray = [];
+		const catNameArray = [];
+		const catIdArray = [];
+		const catUrlArray = [];
+
+		for (var i = 0; i < categoryDetails.length; i++) {
+		    var subNavData = categoryDetails[i].subNav;
+		    for (var j = 0; j < subNavData.length; j++) {
+		        var categoryDetailsData = subNavData[j].attributes;
+		        categoryDetailsArray.push(categoryDetailsData);
+			}
+		}
+
+		for (var k = 0; k < categoryDetailsArray.length; k++) {
+
+			var catName = categoryDetailsArray[k].catName[0];
+			catNameArray.push(catName);
+
+			var catId = categoryDetailsArray[k].catid[0];
+			catIdArray.push(catId);
+
+			var catUrl = categoryDetailsArray[k].catUrl[0];
+			catUrlArray.push(catUrl);
+		}
+
+		this.state = {
+			catNameArray: catNameArray,
+			catIdArray: catIdArray,
+			catUrlArray: catUrlArray
+		};			
+	}
+
 	render() {
 		return(
 			<div className="col-xs-12 col-sm-2 p_lr_init">
@@ -10,22 +48,22 @@ class LeftNavigation extends Component {
 							<dl id="cat0">
 								<dt>Luxewear</dt>
 								<dd>
-									<a href="https://www.anntaylor.com/all-luxewear/cat3750001?https%2525253A%2525252F%2525252Fwww.anntaylor.com%2525252Fall-luxewear%2525252Fcat3750001=undefined"> All Luxewear</a>
+									<a href={this.state.catUrlArray[0]} id={this.state.catIdArray[0]} >{this.state.catNameArray[0]}</a>
 								</dd>
 								<dd>
-									<a href="https://www.anntaylor.com/luxewear-tops/cat3750002?https%2525253A%2525252F%2525252Fwww.anntaylor.com%2525252Fluxewear-tops%2525252Fcat3750002=undefined"> Luxewear Tops + Jackets</a>
+									<a href={this.state.catUrlArray[1]} id={this.state.catIdArray[1]} >{this.state.catNameArray[1]}</a>
 								</dd>
 								<dd>
-									<a href="https://www.anntaylor.com/luxewear-sweaters/cat3800018?https%2525253A%2525252F%2525252Fwww.anntaylor.com%2525252Fluxewear-sweaters%2525252Fcat3800018=undefined"> Luxewear Sweaters</a>
+									<a href={this.state.catUrlArray[2]} id={this.state.catIdArray[2]} >{this.state.catNameArray[2]}</a>
 								</dd>
 								<dd>
-									<a href="https://www.anntaylor.com/luxewear-dresses/cat3800016?https%2525253A%2525252F%2525252Fwww.anntaylor.com%2525252Fluxewear-dresses%2525252Fcat3800016=undefined"> Luxewear Dresses</a>
+									<a href={this.state.catUrlArray[3]} id={this.state.catIdArray[3]} >{this.state.catNameArray[3]}</a>
 								</dd>
 								<dd>
-									<a href="https://www.anntaylor.com/luxewear-bottoms/cat3750003?https%2525253A%2525252F%2525252Fwww.anntaylor.com%2525252Fluxewear-bottoms%2525252Fcat3750003=undefined"> Luxewear Pants + Skirts</a>
+									<a href={this.state.catUrlArray[4]} id={this.state.catIdArray[4]} >{this.state.catNameArray[4]}</a>
 								</dd>
 								<dd>
-									<a href="https://www.anntaylor.com/luxewear-accessories/cat3750004?https%2525253A%2525252F%2525252Fwww.anntaylor.com%2525252Fluxewear-accessories%2525252Fcat3750004=undefined"> Luxewear Accessories</a>
+									<a href={this.state.catUrlArray[5]} id={this.state.catIdArray[5]} >{this.state.catNameArray[5]}</a>
 								</dd>
 							</dl>
 						</div>
